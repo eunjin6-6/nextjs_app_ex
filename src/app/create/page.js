106261d124bfe10493553bbc78c3db6e0b00da34
eunjin6-6 <dmswls616@gmail.com>
2024-11-13 14:21:42 +0900

    사용자 입력내용 전송

--- src/app/create/page.js
+++ src/app/create/page.js
@@ -1,7 +1,39 @@
+"use client"
+
+
 export default function Create() {
+  const onSubmit = (e)=>{
+    e.preventDefault();
+    const title = e.target.title.value;
+    const body = e.target.body.value;
+
+    const options = {
+        method: "POST",
+        headers:{
+          "Content-Type" : "application/json"
+        },
+        body: JSON.stringify({title, body}), //사용자가 입력한 내용을 object->json로 변경
+    }
+    fetch('http://localhost:9999/topics', options)
+      .then(res=>res.json()) //결과를 object 객체로 변환
+      .then(result =>{
+        console.log(result);
+      }); //결과를 object 객체로 변환
+
+  }
+
   return (
     <div>
-      <p>Create!</p>
+      <form onSubmit={onSubmit}>
+        <div>
+          <input type="text" name="title" placeholder="title"></input>
+        </div>
+        <div>
+          <textarea name="body" placeholder="content"></textarea>
+        </div>
+        <button type="submit">전송</button>
+      </form>
+      <hr/>
     </div>
   );
 }
