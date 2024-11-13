b08fe7127be6b54448d26cf35c4e9acf31c88463
eunjin6-6 <dmswls616@gmail.com>
2024-11-13 14:22:21 +0900

    각 topic 내용 출력

--- src/app/read/[id]/page.js
+++ src/app/read/[id]/page.js
@@ -1,8 +1,13 @@
-export default function Read(props) {
+export default async function Read(props) {
+
+  const response = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  const topic = await response.json(); //json->object
+
   return (
-    <div>
-      <h2>Read</h2>
-      <p>parameter : {props.params.id}</p>
+    <div> 
+       <h2>{topic.title}</h2>
+       <p>{topic.body}</p> 
+      {/* <p>parameter : {props.params.id}</p> */}
     </div>
   );
 }
