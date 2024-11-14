6dc4ec6585cdafd9b43aa088c8066138b8294cde
eunjin6-6 <dmswls616@gmail.com>
2024-11-14 10:36:04 +0900

    삭제 기능 추가

--- src/app/read/[id]/Control.js
+++ src/app/read/[id]/Control.js
@@ -1,16 +1,34 @@
 "use client";
 import Link from "next/link";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
+
 
 const Control = ()=>{
   const params = useParams();
   const id = params.id;
+  
+  const router = useRouter(); //라우트 초기화 먼저
+
+  const deleteTopic = ()=>{
+    const option = {method: 'DELETE'};
+
+    fetch('http://localhost:9999/topics/'+id, option)
+    .then(res=>{
+       return res.json();//json->object
+    })
+    .then(result=>{
+      router.push('/'); //성공한 후, 루트로 페이지 이동
+      router.refresh();
+    });
+  }
+
+  
   return(
     <ul>
       <li><Link href="/create">Create</Link></li>
       {id && <>
         <li><Link href={`/update/${id}`}>Update</Link></li>
-        <li><button>delete</button></li>
+        <li><button onClick={deleteTopic}>delete</button></li>
       </>}     
     </ul> 
   )
