554f290f59857f7bd9f53124e0f86e87d4f4c0ff
eunjin6-6 <dmswls616@gmail.com>
2024-11-14 10:15:49 +0900

    글 수정 기능 추가

--- src/app/update/[id]/page.js
+++ src/app/update/[id]/page.js
@@ -1,22 +1,52 @@
 "use client"
 import { useRouter } from 'next/navigation'
+import { useEffect, useState } from 'react';
+import { useParams } from "next/navigation";
 
-export default function Update() {
+export default function Update(props) {
+  const params = useParams();
+  const id = params.id;
+
+  /*
+  서버형 컴포넌트 데이터 조회
+  // const response = await fetch(`http://localhost:9999/topics/${props.params.id}`);
+  // const topic = await response.json(); //json->object
+  */
+
+  //client 컴포넌트에서 데이터 조회
+  //const [topic, setTopic] = useState([]);
+  const [title, setTitle] = useState([]);
+  const [body, setBody] = useState([]);
+
+
+  useEffect(()=>{
+    fetch('http://localhost:9999/topics/'+id)
+    .then(res=>{
+       return res.json();//json->object
+    })
+    .then(result=>{
+      setTitle(result.title);
+      setBody(result.body);
+    });
+  },[id])
+
+
+
   const router = useRouter()
 
   const onSubmit = (e)=>{
     e.preventDefault();
-    const title = e.target.title.value;
-    const body = e.target.body.value;
+    // const title = e.target.title.value;
+    // const body = e.target.body.value;
 
     const options = {
-        method: "POST",
+        method: "PATCH",
         headers:{
           "Content-Type" : "application/json"
         },
         body: JSON.stringify({title, body}), //사용자가 입력한 내용을 object->json로 변경
     }
-    fetch('http://localhost:9999/topics', options)
+    fetch('http://localhost:9999/topics/'+id, options)
       .then(res=>res.json()) //결과를 object 객체로 변환
       .then(result =>{
         console.log(result);
@@ -30,10 +60,25 @@ export default function Update() {
     <div>
       <form onSubmit={onSubmit}>
         <div>
-          <input type="text" name="title" placeholder="title"></input>
+          <input 
+            type="text" 
+            name="title" 
+            value={title} 
+            onChange={(e)=>{
+              setTitle(e.target.value);
+            }} 
+            placeholder="title"
+          ></input>
         </div>
         <div>
-          <textarea name="body" placeholder="content"></textarea>
+          <textarea 
+            name="body" 
+            value={body} 
+            onChange={(e)=>{
+              setBody(e.target.value);
+            }} 
+            placeholder="content"
+          ></textarea>
         </div>
         <button type="submit">전송</button>
       </form>
